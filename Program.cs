6ed29d13fd46925c7a77d53f2322c56ae6a2f140
fkthat <fkthat@yandex.ru>
2024-10-11 13:48:30 +0300

    Ololo!

---
+++ Program.cs
@@ -0,0 +1,56 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.EntityFrameworkCore;
+
+using Db db = new(
+    new DbContextOptionsBuilder<Db>()
+        .UseInMemoryDatabase(Guid.NewGuid().ToString())
+        .Options);
+
+Foo foo = new()
+{
+    Bar = 1000000
+};
+
+db.Foos.Add(foo);
+
+try
+{
+    db.SaveChanges();
+}
+catch (ValidationException e)
+{
+    Console.WriteLine($"Ololo: {e.Message}");
+}
+
+class Foo
+{
+    public int Id { get; set; }
+
+    [Range(0, 1000)]
+    public int Bar { get; set; }
+}
+
+class Db: DbContext
+{
+    public DbSet<Foo> Foos { get; set; }
+
+    public Db(DbContextOptions<Db> options): base(options)
+    {
+    }
+
+    public override int SaveChanges()
+    {
+        ValidateEntities();
+        return base.SaveChanges();
+    }
+
+    private void ValidateEntities()
+    {
+        foreach(var entry in ChangeTracker.Entries().Where(e =>
+            e.State == EntityState.Added || e.State == EntityState.Modified))
+        {
+            var entity = entry.Entity;
+            Validator.ValidateObject(entity, new(entity), validateAllProperties: true);
+        }
+    }
+}
