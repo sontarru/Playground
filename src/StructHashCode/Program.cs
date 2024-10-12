dbfdc544b74a562d63bc2faa802a7d9e222c4794
fkthat <fkthat@yandex.ru>
2024-10-12 14:12:04 +0300

    StructHashCode

---
+++ src/StructHashCode/Program.cs
@@ -0,0 +1,13 @@
+﻿Foo foo1 = new() { Id = 42, Ololo = 73, Name = "foo1" };
+Foo foo2 = new() { Id = 42, Ololo = 51, Name = "foo2" };
+
+// чтозанах - ожидаем false, но получаем true...
+Console.WriteLine(foo1.GetHashCode() == foo2.GetHashCode());
+
+struct Foo
+{
+    public int Id;
+    public int Ololo;
+    public string Name;
+}
+
