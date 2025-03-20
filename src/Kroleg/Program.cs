b838df4f212176c077ef53b62a79b0c853276564
fkthat <fkthat@yandex.ru>
2025-03-20 13:15:24 +0300

    Kroleg

---
+++ src/Kroleg/Program.cs
@@ -0,0 +1,16 @@
+﻿using RabbitMQ.Client;
+
+const string Exchange = "HelloExchange";
+const string Queue = "HelloQueue";
+const string RoutingKey = "HelloRoutingKey";
+
+ConnectionFactory connectionFactory = new()
+{
+    HostName = "localhost"
+};
+
+using var serverConnection = await connectionFactory.CreateConnectionAsync();
+using var serverChannel = await serverConnection.CreateChannelAsync();
+await serverChannel.ExchangeDeclareAsync(Exchange, ExchangeType.Direct);
+await serverChannel.QueueDeclareAsync(Queue);
+await serverChannel.QueueBindAsync(Queue, Exchange, RoutingKey);
