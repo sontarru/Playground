6654a2f8041d7adba4a128fbf9991792b74195f9
fkthat <fkthat@yandex.ru>
2025-03-20 20:45:31 +0300

    TwoEnumerables

---
+++ src/TwoEnumerables/Program.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+
+Ololo ololo = new();
+
+foreach (Foo foo in ololo)
+{
+    Console.WriteLine(foo); // "Foo"
+}
+
+/* Не компилируется
+foreach (Bar bar in ololo)
+{
+    Console.WriteLine(bar);
+}
+*/
+
+foreach (var @var in ololo)
+{
+    Console.WriteLine(@var); // "Foo"
+}
+
+foreach (object obj in ololo)
+{
+    Console.WriteLine(obj); // "Foo"
+}
+
+class Ololo : IEnumerable<Foo>, IEnumerable<Bar>
+{
+    private readonly object[] _objects = [
+      new Foo(), new Bar(),
+      new Foo(), new Bar(),
+      new Foo(), new Bar()
+    ];
+
+    public IEnumerator<Foo> GetEnumerator()
+        => _objects.OfType<Foo>().GetEnumerator();
+
+    IEnumerator<Bar> IEnumerable<Bar>.GetEnumerator()
+        => _objects.OfType<Bar>().GetEnumerator();
+
+    IEnumerator IEnumerable.GetEnumerator()
+        => _objects.OfType<Bar>().GetEnumerator();
+}
+
+class Foo
+{
+}
+
+class Bar
+{
+}
