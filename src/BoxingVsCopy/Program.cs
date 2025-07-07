d3b81a1e65fdd5bd7511a9c2c8e5852925f16774
fkthat <fkthat@yandex.ru>
2025-07-07 07:11:27 +0300

    Update readme. Remove redundant files.

--- src/BoxingVsCopy/Program.cs
+++ src/BoxingVsCopy/Program.cs
@@ -3,7 +3,10 @@
 using BenchmarkDotNet.Attributes;
 using BenchmarkDotNet.Running;
 
-BenchmarkRunner.Run<BoxingVsCopyBenchmark>();
+using Microsoft.Diagnostics.Tracing.AutomatedAnalysis;
+
+// BenchmarkRunner.Run<BoxingVsCopyBenchmark>();
+BenchmarkRunner.Run<NewClassVsStructBenchmark>();
 
 /// <summary>
 /// Benchmarks copy vs. boxing performance.
@@ -43,4 +46,59 @@ public class BoxingVsCopyBenchmark
             _z[i] = _x[i];
         }
     }
+}
+
+sealed class FooClass
+{
+    public FooClass(int id) => Id = id;
+
+    public int Id { get; init; }
+}
+
+struct FooStruct
+{
+    public FooStruct(int id) => Id = id;
+
+    public int Id { get; init; }
+}
+
+/// <summary>
+/// Benchmarks new [class] vs. new [struct].
+/// </summary>
+[SuppressMessage("Design", "CA1050: Declare types in namespaces", Justification =
+    "Not required in a top-level statements program.")]
+[SuppressMessage("Maintainability", "CA1515: Consider making public types internal", Justification =
+    "Should be public to use from the Benchmark.Net.")]
+public class NewClassVsStructBenchmark
+{
+    private const int Count = 1024 * 1024;
+
+    private readonly FooClass[] _classes = new FooClass[Count];
+    private readonly FooStruct[] _structs = new FooStruct[Count];
+
+    /// <summary>
+    /// Benchmarks new [class].
+    /// </summary>
+    [Benchmark]
+    public void NewClass()
+    {
+        for(var i = 0; i < Count; i++)
+        {
+            _classes[i] = new FooClass(i);
+        }
+    }
+
+    /// <summary>
+    /// Benchmarks new [struct].
+    /// </summary>
+    [Benchmark]
+    public void NewStruct()
+    {
+        for(var i = 0; i < Count; i++)
+        {
+            _structs[i] = NewStruct(i);
+        }
+    }
+
+    private static FooStruct NewStruct(int i) => new FooStruct(i);
 }
